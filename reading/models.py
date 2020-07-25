7a641599abbd031c1e4a6f05cf2b2731cce5f42b
Adrià Sala <adriasala124@gmail.com>
2020-07-25 23:42:32 +0200

    Added followers to user model

--- reading/models.py
+++ reading/models.py
@@ -2,4 +2,4 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    pass
+    following = models.ManyToManyField('self', symmetrical=False, related_name='followers')
