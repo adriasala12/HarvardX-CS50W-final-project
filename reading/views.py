e286e0e5ad4c66a0627f32e49d7831c34e2cdecc
Adrià Sala <adriasala124@gmail.com>
2020-07-25 21:02:32 +0200

    Added login functionality

--- reading/views.py
+++ reading/views.py
@@ -1,4 +1,24 @@
 from django.shortcuts import render
+from django.contrib.auth import login, logout, authenticate
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 def index(request):
     return render(request, "reading/index.html")
+
+
+def login_view(request):
+    if request.method == 'POST':
+
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse('index'))
+        else:
+            return render(request, "reading/login.html", {'message': "Invalid username and/or password"})
+    
+    else:
+        return render(request, "reading/login.html")
