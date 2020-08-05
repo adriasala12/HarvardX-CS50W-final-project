39380202e368ecabd2190af8c40cc107ba228794
Adrià Sala <adriasala124@gmail.com>
2020-08-05 18:08:28 +0200

    Added Book class to save data from search. Search creates array of books that passes 2 view

--- reading/aux.py
+++ reading/aux.py
@@ -3,10 +3,39 @@ import urllib
 
 apiKey = 'AIzaSyDFfXEM1ApgkSRCkUnnFBlgk67D6DS9fhU'
 
+class Book():
+
+    def __init__(self, gid, title, authors, publisher, pubdate, pages, thumbnail):
+        self.gid = gid
+        self.title = title
+        self.authors = authors
+        self.publisher = publisher
+        self.pubdate = pubdate
+        self.pages = pages
+        self.thumbnail = thumbnail
+
+
 def getBooksByTitle(title):
     
     query = urllib.parse.quote(title)
-    print(f"query: {query}")
     response = requests.get(f'https://www.googleapis.com/books/v1/volumes?q={query}&key={apiKey}')
     data = response.json()
-    print(data['items'][0])
+    
+    books = []
+
+    for item in data['items']:
+
+        try:
+            book = Book(item['id'], item['volumeInfo']['title'], 
+                item['volumeInfo']['authors'], 
+                item['volumeInfo']['publisher'], 
+                item['volumeInfo']['publishedDate'],
+                item['volumeInfo']['pageCount'],
+                item['volumeInfo']['imageLinks']['thumbnail'])
+
+            books.append(book)
+        except:
+            pass
+
+    return books
+        
