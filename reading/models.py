2a02b5107eaab9fb4dd3ba613a1ea3cf2a03b7b0
Adrià Sala <adriasala124@gmail.com>
2020-08-09 13:12:00 +0200

    User doesn't have readinglist but book is assigned to a user with foreignKey

--- reading/models.py
+++ reading/models.py
@@ -2,16 +2,16 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
+class User(AbstractUser):
+    following = models.ManyToManyField('self', symmetrical=False, related_name='followers')
+    image = models.ImageField(default='static/reading/resources/image_placeholder.png')
+
+
 class Book(models.Model):
     gid = models.CharField(max_length=300)
     started = models.DateField(null=True)
     finished = models.DateField(null=True)
-
-
-class User(AbstractUser):
-    following = models.ManyToManyField('self', symmetrical=False, related_name='followers')
-    readinglist = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='read_by')
-    image = models.ImageField(default='static/reading/resources/image_placeholder.png')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='readingList')
 
 
 class Comment(models.Model):
