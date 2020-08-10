e871f8c7e5ed7633d5b5fcacbdc8d5a59f0c870d
Adrià Sala <adriasala124@gmail.com>
2020-08-10 13:30:37 +0200

    Linked js file to user page to handle which books are shown

---
+++ reading/static/reading/js/readinglist.js
@@ -0,0 +1,16 @@
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    console.log('hi')
+
+    document.querySelectorAll('.book').forEach(book => {
+        book.style.display = 'none'
+    });
+
+
+
+
+
+
+});
