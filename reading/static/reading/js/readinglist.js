65f1f9e2bd1a8ac23049fb59012362ba43fe43c9
Adrià Sala <adriasala124@gmail.com>
2020-08-10 14:54:55 +0200

    Saved,reading,read buttons show the corresponding books and change state

--- reading/static/reading/js/readinglist.js
+++ reading/static/reading/js/readinglist.js
@@ -1,16 +1,56 @@
 
-
 document.addEventListener('DOMContentLoaded', () => {
 
-    console.log('hi')
+    const btn_saved = document.getElementById('btn-saved');
+    const btn_reading = document.getElementById('btn-reading');
+    const btn_read = document.getElementById('btn-read');
 
-    document.querySelectorAll('.book').forEach(book => {
-        book.style.display = 'none'
-    });
+    btn_saved.onclick = () => {
+        btn_saved.classList.add('active');
+        btn_reading.classList.remove('active');
+        btn_read.classList.remove('active');
 
+        document.querySelectorAll('.saved').forEach(book => {
+            book.style.display = 'block'
+        });
+        document.querySelectorAll('.reading').forEach(book => {
+            book.style.display = 'none'
+        });
+        document.querySelectorAll('.read').forEach(book => {
+            book.style.display = 'none'
+        });
+    };
 
+    btn_reading.onclick = () => {
+        btn_saved.classList.remove('active');
+        btn_reading.classList.add('active');
+        btn_read.classList.remove('active');
 
+        document.querySelectorAll('.saved').forEach(book => {
+            book.style.display = 'none'
+        });
+        document.querySelectorAll('.reading').forEach(book => {
+            book.style.display = 'block'
+        });
+        document.querySelectorAll('.read').forEach(book => {
+            book.style.display = 'none'
+        });
+    };
 
+    btn_read.onclick = () => {
+        btn_saved.classList.remove('active');
+        btn_reading.classList.remove('active');
+        btn_read.classList.add('active');
 
+        document.querySelectorAll('.saved').forEach(book => {
+            book.style.display = 'none'
+        });
+        document.querySelectorAll('.reading').forEach(book => {
+            book.style.display = 'none'
+        });
+        document.querySelectorAll('.read').forEach(book => {
+            book.style.display = 'block'
+        });
+    };
 
 });
